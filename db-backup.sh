e5bb3d98f61395a9633ef053bb42734e57e88a58
Engin ALTAY <43572692+enginaltay@users.noreply.github.com>
2018-12-06 11:14:03 +0300

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
-tar -zcvf definition-test-db-`date +"%Y-%m-%d"`.tar.gz /mnt/definition-test-db/
-tar -xvf definition-test-db-`date +"%Y-%m-%d"`.tar.gz -C /db-backup --strip 1
+tar -zcvf test-db-`date +"%Y-%m-%d"`.tar.gz /mnt/test-db/ #Tar db folder
+tar -xvf test-db-`date +"%Y-%m-%d"`.tar.gz -C /db-backup --strip 1 #untar db to backup folder
